e0e3103115b487a029faae566cfd73a5161ec7b3
Evgenii3113 <salutbel@mail.ru>
2021-02-26 13:22:29 +0300

    commit 260221

--- src/main/java/evk/itmo/fprj/web/SalutController.java
+++ src/main/java/evk/itmo/fprj/web/SalutController.java
@@ -1,11 +1,22 @@
 package evk.itmo.fprj.web;
 
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class SalutController {
 
+    @GetMapping({"/", "/hello/{name}"})
+    public String hello(Model model, @PathVariable String name) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
 
+    @RequestMapping(value = "/translate", method = RequestMethod.POST)
+    public String translate(@RequestParam String textForm, Model model) {
+        model.addAttribute("textForm", textForm);
+        return "success";
+    }
 
 }
